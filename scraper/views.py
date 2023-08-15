c19d30bd86e14840bc0076a482e850eff93b9e8a
Yazid <yazeedkurdi@hotmail.com>
2023-08-15 23:32:56 +0300

    add list api view with pagination/filters

--- scraper/views.py
+++ scraper/views.py
@@ -1,8 +1,13 @@
+from rest_framework.filters import SearchFilter
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.generics import ListAPIView
+from rest_framework.permissions import IsAuthenticated
 
-from .serializers import KeywordSerializer
+from scraper import StandardResultsSetPagination
+from .models import ScrapeResult
+from .serializers import KeywordSerializer,ScrapeResultSerializer
 from .scraper import EcommerceScraper
 
 
@@ -23,3 +28,15 @@ class ProductScrapeView(APIView):
 
         return Response(scraped_data, status=status.HTTP_200_OK)
 
+class UserScrapeResultListView(ListAPIView):
+
+    serializer_class = ScrapeResultSerializer
+    permission_classes = [IsAuthenticated]
+    pagination_class = StandardResultsSetPagination
+    filter_backends = [SearchFilter]  # Add SearchFilter backend
+    search_fields = ['product_title']  # Specify fields to search
+
+    def get_queryset(self):
+        user = self.request.user
+        queryset = ScrapeResult.objects.filter(user=user)
+        return queryset
