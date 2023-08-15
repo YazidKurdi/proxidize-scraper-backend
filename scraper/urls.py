0f2cb41f9f71efff02005d62b22ab1d06c6da3d0
Yazid <yazeedkurdi@hotmail.com>
2023-08-15 23:33:13 +0300

    list view url

--- scraper/urls.py
+++ scraper/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import ProductScrapeView
+from .views import ProductScrapeView, UserScrapeResultListView
 
 urlpatterns = [
     path('scrape/', ProductScrapeView.as_view()),
+    path('user/scrape-results/', UserScrapeResultListView.as_view())
 ]
