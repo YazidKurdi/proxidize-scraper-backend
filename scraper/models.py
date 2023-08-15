907539be28c626bcaa86216c140745b6f667d6c1
Yazid <yazeedkurdi@hotmail.com>
2023-08-15 18:39:55 +0300

    scraper results model

--- scraper/models.py
+++ scraper/models.py
@@ -1,3 +1,18 @@
+# scraper/models.py
 from django.db import models
 
-# Create your models here.
+class ScrapeResult(models.Model):
+
+    custom_id = models.CharField(max_length=255, unique=True)
+    product_title = models.CharField(max_length=200)
+    image = models.URLField()
+    info = models.TextField()
+    price = models.CharField(max_length=50)
+
+
+    def save(self, *args, **kwargs):
+        self.custom_id = f"{self.product_title}_{self.price}_{self.image}"
+        super(ScrapeResult, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.product_title
