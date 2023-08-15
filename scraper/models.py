5f676fb90594b320969e891e12e89e227bf13fb6
Yazid <yazeedkurdi@hotmail.com>
2023-08-15 23:36:53 +0300

    update model null,blank + update save method

--- scraper/models.py
+++ scraper/models.py
@@ -1,17 +1,21 @@
 # scraper/models.py
 from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 class ScrapeResult(models.Model):
 
     custom_id = models.CharField(max_length=255, unique=True)
     product_title = models.CharField(max_length=200)
-    image = models.URLField()
-    info = models.TextField()
-    price = models.CharField(max_length=50)
+    image = models.URLField(null=True,blank=True)
+    info = models.TextField(null=True,blank=True)
+    price = models.CharField(max_length=50,null=True,blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
 
     def save(self, *args, **kwargs):
-        self.custom_id = f"{self.product_title}_{self.price}_{self.image}"
+        self.custom_id = f"{self.product_title}_{self.price}_{self.image}_{self.user.id}"
         super(ScrapeResult, self).save(*args, **kwargs)
 
     def __str__(self):
