8ca822c932408bb5b67bb6d7b8b2fcbe8c61abd1
Yazid <yazeedkurdi@hotmail.com>
2023-08-17 21:39:37 +0300

    include page_size in response

--- scraper/__init__.py
+++ scraper/__init__.py
@@ -3,3 +3,8 @@ from rest_framework.pagination import PageNumberPagination
 class StandardResultsSetPagination(PageNumberPagination):
     page_size = 5
     page_size_query_param = 'page_size'
+
+    def get_paginated_response(self, data):
+        response = super().get_paginated_response(data)
+        response.data['page_size'] = self.page_size
+        return response
