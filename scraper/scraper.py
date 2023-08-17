6a1a15acaab3eba7647833a658363ef7368372e0
Yazid <yazeedkurdi@hotmail.com>
2023-08-17 21:39:12 +0300

    doc strings

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -13,59 +13,12 @@ from scraper.serializers import ScrapeResultSerializer
 
 class EcommerceScraper:
     def __init__(self):
+        """
+        Initializes an instance of the EcommerceScraper class with a Chrome WebDriver.
+        The WebDriver is used to interact with a web browser and scrape data from a website.
+        """
         self.driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
         self.driver.maximize_window()
-
-    def get_elements_to_scrape(self, keyword, min_rows_to_scrape):
-        url = f'https://www.jarir.com/sa-en/catalogsearch/result?search={keyword}'
-        self.driver.get(url)
-        wait = WebDriverWait(self.driver, 10)
-        wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'button__contrylangSelector')))
-
-        self.driver.find_element(By.CLASS_NAME, 'button__contrylangSelector').click()
-        time.sleep(1)
-        self.driver.find_element(By.CLASS_NAME, 'p16').click()
-
-
-        vue_carousel_element = wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'VueCarousel')))
-        last_height = self.driver.execute_script("return document.body.scrollHeight")
-
-        while True:
-            elements = self.driver.find_elements(By.CLASS_NAME, 'product-tile__item--spacer')[12:]
-            if len(elements) >= min_rows_to_scrape:
-                break
-
-            # Scroll down to the bottom of the page
-            self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-            time.sleep(2)  # You can adjust the sleep time as needed
-
-            new_height = self.driver.execute_script("return document.body.scrollHeight")
-            if new_height == last_height:
-                break
-            last_height = new_height
-
-        # Now that scrolling is done, get the desired number of elements to scrape
-        return elements[:min(min_rows_to_scrape, len(elements))]
-
-    def save_scrape_result(self, data,request):
-        product_title = data["product_title"]
-        img_element = data["image"]
-        info_texts = data["info"]
-        price_full = data["price"]
-        custom_id = f"{product_title}_{price_full}_{img_element}_{request.user.id}"
-
-        instance_data = {
-            **data,
-            "custom_id": custom_id,
-            "user": request.user.id,
-        }
-
-        serializer = ScrapeResultSerializer(data=instance_data)
-        if serializer.is_valid():
-            if not ScrapeResult.objects.filter(custom_id=custom_id).exists():
-                serializer.save(custom_id=custom_id)
-                return True
-        return False
 
     def scrape_website(self, request,keyword,rows):
         try:
@@ -109,38 +62,45 @@ class EcommerceScraper:
         finally:
             self.driver.quit()
 
-    # def get_elements_to_scrape(self, keyword):
-    #     url = f'https://www.jarir.com/sa-en/catalogsearch/result?search={keyword}'
-    #     self.driver.get(url)
-    #     wait = WebDriverWait(self.driver, 10)
-    #     vue_carousel_element = wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'VueCarousel')))
-    #     for element in self.driver.find_elements(By.CLASS_NAME,'product-tile__item--spacer')[12:]:
-    #         print(element.find_element(By.CLASS_NAME,'product-title__title').text)
-    #     return self.driver.find_elements(By.CLASS_NAME, 'product-tile__item--spacer')[12:]
+    def get_elements_to_scrape(self, keyword, min_rows_to_scrape):
+        """
+        Scrapes and returns a list of elements containing product information based on a search keyword.
+        """
 
-    # def extract_product_info(self, element):
-    #
-    #     wait = WebDriverWait(element, 10)
-    #
-    #     title_element = wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'product-title__title'))).get_attribute("textContent").strip()
-    #     img_element = wait.until(
-    #         EC.presence_of_all_elements_located((By.CSS_SELECTOR, '.image.image--contain')))[2].get_attribute("src")
-    #     tag_element = element.find_element(By.CLASS_NAME, 'product-title__info')
-    #
-    #     if tag_element:
-    #         info_texts = [info_span.text.strip() for info_span in tag_element.find_elements(By.TAG_NAME, 'span')]
-    #     else:
-    #         info_texts = []
-    #
-    #     price_element = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '.price_alignment')))
-    #     price_currency = price_element.find_element(By.CLASS_NAME, 'price__currency').get_attribute(
-    #         "textContent").strip()
-    #     price_amount = price_element.find_element(By.XPATH, './span[2]').get_attribute("textContent").strip()
-    #     price_full = f"{price_currency} {price_amount}"
-    #
-    #     return title_element, img_element, info_texts, price_full
+        url = f'https://www.jarir.com/sa-en/catalogsearch/result?search={keyword}'
+        self.driver.get(url)
+        wait = WebDriverWait(self.driver, 10)
+        wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'button__contrylangSelector')))
+
+        self.driver.find_element(By.CLASS_NAME, 'button__contrylangSelector').click()
+        time.sleep(1)
+        self.driver.find_element(By.CLASS_NAME, 'p16').click()
+
+
+        vue_carousel_element = wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'VueCarousel')))
+        last_height = self.driver.execute_script("return document.body.scrollHeight")
+
+        while True:
+            elements = self.driver.find_elements(By.CLASS_NAME, 'product-tile__item--spacer')[12:]
+            if len(elements) >= min_rows_to_scrape:
+                break
+
+            # Scroll down to the bottom of the page
+            self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            time.sleep(2)
+
+            new_height = self.driver.execute_script("return document.body.scrollHeight")
+            if new_height == last_height:
+                break
+            last_height = new_height
+
+        # Get the desired number of elements to scrape
+        return elements[:min(min_rows_to_scrape, len(elements))]
 
     def extract_product_info(self, element):
+        """
+        Extracts product information from a given element.
+        """
         title_element = element.find_element(By.CLASS_NAME, 'product-title__title').get_attribute("textContent").strip()
         img_elements = element.find_elements(By.CSS_SELECTOR, '.image.image--contain')
         img_element = img_elements[2].get_attribute("src")
@@ -158,3 +118,26 @@ class EcommerceScraper:
         price_full = f"{price_currency} {price_amount}"
 
         return title_element, img_element, info_texts, price_full
+
+    def save_scrape_result(self, data,request):
+        """
+        Saves the scraped product data to the database if does not already exist.
+        """
+        product_title = data["product_title"]
+        img_element = data["image"]
+        info_texts = data["info"]
+        price_full = data["price"]
+        custom_id = f"{product_title}_{price_full}_{img_element}_{request.user.id}"
+
+        instance_data = {
+            **data,
+            "custom_id": custom_id,
+            "user": request.user.id,
+        }
+
+        serializer = ScrapeResultSerializer(data=instance_data)
+        if serializer.is_valid():
+            if not ScrapeResult.objects.filter(custom_id=custom_id).exists():
+                serializer.save(custom_id=custom_id)
+                return True
+        return False
