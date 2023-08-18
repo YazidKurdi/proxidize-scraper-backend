0eeda9db9c1e15d77386eec391b88a42290d6273
Yazid <yazeedkurdi@hotmail.com>
2023-08-18 20:01:26 +0300

    remove windows size test memory

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -23,9 +23,7 @@ class EcommerceScraper:
         chrome_options.add_argument("--disable-dev-shm-usage")
         chrome_options.add_argument('--no-sandbox')
         chrome_options.add_argument("--disable-gpu")
-        ## might not be needed
         chrome_options.add_argument("--remote-debugging-port=9222")
-        chrome_options.add_argument('--window-size=1920x1480')
         self.driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=chrome_options)
 
     def scrape_website(self, request,keyword,rows):
