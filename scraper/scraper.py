6b45a7153b8e70d9ea38d136650878dcc796713c
Yazid <yazeedkurdi@hotmail.com>
2023-08-18 21:48:33 +0300

    resume headless mode

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -19,11 +19,11 @@ class EcommerceScraper:
         The WebDriver is used to interact with a web browser and scrape data from a website.
         """
         chrome_options = Options()
-        # chrome_options.add_argument("--headless")
-        # chrome_options.add_argument("--disable-dev-shm-usage")
-        # chrome_options.add_argument('--no-sandbox')
-        # chrome_options.add_argument("--disable-gpu")
-        # chrome_options.add_argument("--remote-debugging-port=9222")
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--disable-dev-shm-usage")
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument("--disable-gpu")
+        chrome_options.add_argument("--remote-debugging-port=9222")
         self.driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=chrome_options)
 
     def scrape_website(self, request,keyword,rows):
